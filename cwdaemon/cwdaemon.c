200784b60fec63b21aa3c1672f5651b71adf34e2
Christoph Berg <myon@debian.org>
2020-06-06 22:42:07 +0200

    cwdaemon: Support reading speed and paddle status from digispark

--- cwdaemon/cwdaemon.c
+++ cwdaemon/cwdaemon.c
@@ -2,7 +2,7 @@
  * CW keyer frontend for https://github.com/df7cb/df7cb-shack/tree/master/digispark_keyer
  * Compatible with cwdaemon
  *
- * Copyright (C) 2019 Christoph Berg DF7CB <cb@df7cb.de>
+ * Copyright (C) 2019-2020 Christoph Berg DF7CB <cb@df7cb.de>
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the “Software”), to deal
@@ -26,6 +26,7 @@
  * udpserver.c - A simple UDP echo server
  */
 
+#include <errno.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
@@ -39,7 +40,7 @@
 #include <fcntl.h>
 #include <ctype.h>
 
-#define DEBUG 0
+#define DEBUG 1
 #define BUFSIZE 1024
 #define PORTNO 6789
 #define DELAY 5000 /* sleep 5ms between signals. keep short so using the paddles to abort sending works */
@@ -107,14 +108,110 @@ void error(char *msg)
 	exit(1);
 }
 
+static void
+read_char(unsigned char c)
+{
+	static int c0 = -1;
+
+	if (c0 != -1) {
+		printf("Speed is %d (%d %d)\n", 256*c0 + c, c0, c);
+		c0 = -1;
+		return;
+	}
+
+	switch (c) {
+		case 0:
+		case 1:
+			printf("%d\n", c);
+			break;
+		case 2:
+		case 3:
+			c0 = c;
+			break;
+		default:
+			printf("Unexpected char %d (c0 %d)\n", c, c0);
+	}
+}
+
+void
+read_digispark(int ttyfd)
+{
+	char buf[BUFSIZE];
+	int n;
+
+	n = read(ttyfd, buf, BUFSIZE);
+	if (n < 0)
+		error("read");
+	if (n == 0) {
+		printf("Digispark disconnected\n");
+		exit(0);
+	}
+
+	buf[n] = '\0';
+	for (int i = 0; i < n; i++)
+		read_char(buf[i]);
+}
+
+void
+send_cw(int sockfd, int ttyfd)
+{
+	char buf[BUFSIZE];	/* message buf */
+	int n;			/* message byte size */
+
+	if (sockfd == 0)
+		n = read(sockfd, buf, BUFSIZE);
+	else
+		n = recv(sockfd, buf, BUFSIZE, 0);
+	if (n < 0)
+		error("ERROR in recvfrom");
+	if (n == 0) /* skip empty packets */
+		return;
+
+	if (buf[0] == '\033') {
+		switch (buf[1]) {
+			case '4': /* abort current message */
+				if (DEBUG) printf("Abort\n");
+				if (write(ttyfd, "*", 1) != 1)
+					error("write");
+				break;
+			default:
+				buf[n] = '\0';
+				printf("Unknown ESC message: %s\n", buf + 1);
+				break;
+		}
+		return;
+	}
+
+	buf[n-1] = '\0';
+	if (n > 1 && buf[n-2] == '\n')
+		buf[n-2] = '\0';
+	if (DEBUG) printf("Sending '%s'\n", buf);
+
+	for (int i = 0; i < n; i++) {
+		if (buf[i] == '\0')
+			return;
+		//write(1, buf + i, 1);
+		char *code = morse_char(buf[i]);
+		if (code) {
+			for (int j = 0; code[j]; j++) {
+				//write(1, code + j, 1);
+				if (write(ttyfd, code + j, 1) != 1)
+					error("write");
+				usleep(DELAY);
+			}
+		} else {
+			printf("Unknown symbol, cannot send: '%c'\n", buf[i]);
+		}
+	}
+	if (DEBUG) printf("Done with '%s'\n", buf);
+}
+
 int main(int argc, char **argv)
 {
 	int sockfd;		/* socket */
 	int ttyfd;
 	struct sockaddr_in serveraddr;	/* server's addr */
-	char buf[BUFSIZE];	/* message buf */
 	int optval;		/* flag value for setsockopt */
-	int n;			/* message byte size */
 
 	setlinebuf(stdout);
 
@@ -123,8 +220,17 @@ int main(int argc, char **argv)
 		exit(1);
 	}
 
-	if (!(ttyfd = open(argv[1], O_WRONLY)))
-		error("Could not open tty");
+	do {
+		ttyfd = open(argv[1], O_RDWR);
+		if (ttyfd < 0) {
+			if (errno == EBUSY) {
+				perror(argv[1]);
+				sleep(1);
+			} else
+				error(argv[1]);
+		}
+	} while (ttyfd < 0);
+	printf("Connected to %s\n", argv[1]);
 
 	/*
 	 * socket: create the parent socket
@@ -156,53 +262,25 @@ int main(int argc, char **argv)
 	if (bind(sockfd, (struct sockaddr *)&serveraddr,
 		 sizeof(serveraddr)) < 0)
 		error("ERROR on binding");
+	printf("Listening on :%d\n", PORTNO);
 
 	/*
 	 * main loop: wait for a datagram, then send it to keyer
 	 */
 	while (1) {
-		n = recv(sockfd, buf, BUFSIZE, 0);
-		if (n < 0)
-			error("ERROR in recvfrom");
-		if (n == 0) /* skip empty packets */
-			continue;
+		fd_set fds;
+		FD_ZERO(&fds);
+		FD_SET(ttyfd, &fds);
+		FD_SET(0, &fds);
+		FD_SET(sockfd, &fds);
 
-		if (buf[0] == '\033') {
-			switch (buf[1]) {
-				case '4': /* abort current message */
-					if (DEBUG) printf("Abort\n");
-					if (write(ttyfd, "*", 1) != 1)
-						error("write");
-					break;
-				default:
-					buf[n] = '\0';
-					printf("Unknown ESC message: %s\n", buf + 1);
-					break;
-			}
-			continue;
-		}
-
-		buf[n-1] = '\0';
-		if (n > 1 && buf[n-2] == '\n')
-			buf[n-2] = '\0';
-		if (DEBUG) printf("Sending '%s'\n", buf);
-
-		for (int i = 0; i < n; i++) {
-			if (buf[i] == '\0')
-				continue;
-			//write(1, buf + i, 1);
-			char *code = morse_char(buf[i]);
-			if (code) {
-				for (int j = 0; code[j]; j++) {
-					//write(1, code + j, 1);
-					if (write(ttyfd, code + j, 1) != 1)
-						error("write");
-					usleep(DELAY);
-				}
-			} else {
-				printf("Unknown symbol, cannot send: '%c'\n", buf[i]);
-			}
-		}
-		if (DEBUG) printf("Done with '%s'\n", buf);
+		if (select(sockfd + 1, &fds, NULL, NULL, NULL) < 0)
+			error("select");
+		if (FD_ISSET(ttyfd, &fds))
+			read_digispark(ttyfd);
+		if (FD_ISSET(0, &fds))
+			send_cw(0, ttyfd);
+		if (FD_ISSET(sockfd, &fds))
+			send_cw(sockfd, ttyfd);
 	}
 }
