aa05d7bf1df0aa1b2d184abcb611abdb09df4f09
Christoph Berg <myon@debian.org>
2022-01-21 22:25:26 +0100

    qo100: frequency sink

--- gnuradio/qo100.py
+++ gnuradio/qo100.py
@@ -144,6 +144,48 @@ class qo100(gr.top_block, Qt.QWidget):
         self._qtgui_waterfall_sink_x_0_win = sip.wrapinstance(self.qtgui_waterfall_sink_x_0.qwidget(), Qt.QWidget)
 
         self.top_layout.addWidget(self._qtgui_waterfall_sink_x_0_win)
+        self.qtgui_freq_sink_x_1 = qtgui.freq_sink_c(
+            1024, #size
+            window.WIN_HAMMING, #wintype
+            0, #fc
+            48e3, #bw
+            "", #name
+            1,
+            None # parent
+        )
+        self.qtgui_freq_sink_x_1.set_update_time(0.01)
+        self.qtgui_freq_sink_x_1.set_y_axis(-85, -40)
+        self.qtgui_freq_sink_x_1.set_y_label('Relative Gain', 'dB')
+        self.qtgui_freq_sink_x_1.set_trigger_mode(qtgui.TRIG_MODE_FREE, 0.0, 0, "")
+        self.qtgui_freq_sink_x_1.enable_autoscale(False)
+        self.qtgui_freq_sink_x_1.enable_grid(True)
+        self.qtgui_freq_sink_x_1.set_fft_average(0.1)
+        self.qtgui_freq_sink_x_1.enable_axis_labels(True)
+        self.qtgui_freq_sink_x_1.enable_control_panel(True)
+        self.qtgui_freq_sink_x_1.set_fft_window_normalized(True)
+
+
+
+        labels = ['', '', '', '', '',
+            '', '', '', '', '']
+        widths = [1, 1, 1, 1, 1,
+            1, 1, 1, 1, 1]
+        colors = ["blue", "red", "green", "black", "cyan",
+            "magenta", "yellow", "dark red", "dark green", "dark blue"]
+        alphas = [1.0, 1.0, 1.0, 1.0, 1.0,
+            1.0, 1.0, 1.0, 1.0, 1.0]
+
+        for i in range(1):
+            if len(labels[i]) == 0:
+                self.qtgui_freq_sink_x_1.set_line_label(i, "Data {0}".format(i))
+            else:
+                self.qtgui_freq_sink_x_1.set_line_label(i, labels[i])
+            self.qtgui_freq_sink_x_1.set_line_width(i, widths[i])
+            self.qtgui_freq_sink_x_1.set_line_color(i, colors[i])
+            self.qtgui_freq_sink_x_1.set_line_alpha(i, alphas[i])
+
+        self._qtgui_freq_sink_x_1_win = sip.wrapinstance(self.qtgui_freq_sink_x_1.qwidget(), Qt.QWidget)
+        self.top_layout.addWidget(self._qtgui_freq_sink_x_1_win)
         self.limesdr_source_0 = limesdr.source('', 0, '')
         self.limesdr_source_0.set_sample_rate(samp_rate)
         self.limesdr_source_0.set_center_freq(739.75e6, 0)
@@ -163,7 +205,7 @@ class qo100(gr.top_block, Qt.QWidget):
         self.blocks_multiply_xx_0_0_0_0 = blocks.multiply_vcc(1)
         self.blocks_multiply_xx_0_0_0 = blocks.multiply_vcc(1)
         self.blocks_multiply_const_vxx_1 = blocks.multiply_const_cc(0.01)
-        self.blocks_multiply_const_vxx_0_0 = blocks.multiply_const_ff(50)
+        self.blocks_multiply_const_vxx_0_0 = blocks.multiply_const_ff(10)
         self.blocks_multiply_const_vxx_0 = blocks.multiply_const_ff(af_gain)
         self.blocks_float_to_complex_1_0 = blocks.float_to_complex(1)
         self.blocks_complex_to_float_0_0 = blocks.complex_to_float(1)
@@ -235,6 +277,7 @@ class qo100(gr.top_block, Qt.QWidget):
         self.connect((self.limesdr_source_0, 0), (self.blocks_multiply_xx_0_0_0_0, 0))
         self.connect((self.limesdr_source_0, 0), (self.blocks_multiply_xx_0_0_0_0_0, 0))
         self.connect((self.rational_resampler_xxx_0, 0), (self.blocks_complex_to_float_0, 0))
+        self.connect((self.rational_resampler_xxx_0, 0), (self.qtgui_freq_sink_x_1, 0))
         self.connect((self.rational_resampler_xxx_0_0, 0), (self.blocks_multiply_xx_0_0_0, 0))
         self.connect((self.rational_resampler_xxx_0_1, 0), (self.blocks_complex_to_float_0_0, 0))
 
