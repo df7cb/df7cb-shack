5bc03e8a41ea33b48b7b336f9c6332ea4d429e02
Christoph Berg <myon@debian.org>
2022-01-21 23:36:26 +0100

    qo100: Set sample rate to 11*48kHz

--- gnuradio/qo100.py
+++ gnuradio/qo100.py
@@ -80,8 +80,9 @@ class qo100(gr.top_block, Qt.QWidget):
         ##################################################
         # Variables
         ##################################################
+        self.decim = decim = 11
         self.vfo = vfo = 40
-        self.samp_rate = samp_rate = 600000
+        self.samp_rate = samp_rate = decim*48e3
         self.mag = mag = 0.9
         self.af_gain = af_gain = 50
 
@@ -90,25 +91,23 @@ class qo100(gr.top_block, Qt.QWidget):
         ##################################################
         self._vfo_range = Range(-10, 510, 0.1, 40, 150)
         self._vfo_win = RangeWidget(self._vfo_range, self.set_vfo, "vfo", "counter_slider", float, QtCore.Qt.Horizontal)
-        self.top_layout.addWidget(self._vfo_win)
-        self._af_gain_range = Range(0, 200, 1, 50, 200)
+        self.top_grid_layout.addWidget(self._vfo_win, 14, 0, 1, 1)
+        for r in range(14, 15):
+            self.top_grid_layout.setRowStretch(r, 1)
+        for c in range(0, 1):
+            self.top_grid_layout.setColumnStretch(c, 1)
+        self._af_gain_range = Range(0, 200, 5, 50, 200)
         self._af_gain_win = RangeWidget(self._af_gain_range, self.set_af_gain, "af_gain", "counter_slider", float, QtCore.Qt.Horizontal)
-        self.top_layout.addWidget(self._af_gain_win)
-        self.rational_resampler_xxx_0_1 = filter.rational_resampler_ccf(
-                interpolation=2,
-                decimation=25,
+        self.top_grid_layout.addWidget(self._af_gain_win, 29, 0, 1, 1)
+        for r in range(29, 30):
+            self.top_grid_layout.setRowStretch(r, 1)
+        for c in range(0, 1):
+            self.top_grid_layout.setColumnStretch(c, 1)
+        self.rational_resampler_xxx_0 = filter.rational_resampler_ccc(
+                interpolation=decim,
+                decimation=1,
                 taps=[],
-                fractional_bw=0.4)
-        self.rational_resampler_xxx_0_0 = filter.rational_resampler_ccf(
-                interpolation=25,
-                decimation=2,
-                taps=[],
-                fractional_bw=0.002)
-        self.rational_resampler_xxx_0 = filter.rational_resampler_ccf(
-                interpolation=2,
-                decimation=25,
-                taps=[],
-                fractional_bw=0.4)
+                fractional_bw=0)
         self.qtgui_waterfall_sink_x_0 = qtgui.waterfall_sink_c(
             2048, #size
             window.WIN_BLACKMAN_hARRIS, #wintype
@@ -143,27 +142,32 @@ class qo100(gr.top_block, Qt.QWidget):
 
         self._qtgui_waterfall_sink_x_0_win = sip.wrapinstance(self.qtgui_waterfall_sink_x_0.qwidget(), Qt.QWidget)
 
-        self.top_layout.addWidget(self._qtgui_waterfall_sink_x_0_win)
+        self.top_grid_layout.addWidget(self._qtgui_waterfall_sink_x_0_win, 0, 0, 14, 1)
+        for r in range(0, 14):
+            self.top_grid_layout.setRowStretch(r, 1)
+        for c in range(0, 1):
+            self.top_grid_layout.setColumnStretch(c, 1)
         self.qtgui_freq_sink_x_1 = qtgui.freq_sink_c(
             1024, #size
             window.WIN_HAMMING, #wintype
             0, #fc
-            48e3, #bw
+            24e3, #bw
             "", #name
             1,
             None # parent
         )
         self.qtgui_freq_sink_x_1.set_update_time(0.01)
-        self.qtgui_freq_sink_x_1.set_y_axis(-85, -40)
+        self.qtgui_freq_sink_x_1.set_y_axis(-86, -40)
         self.qtgui_freq_sink_x_1.set_y_label('Relative Gain', 'dB')
         self.qtgui_freq_sink_x_1.set_trigger_mode(qtgui.TRIG_MODE_FREE, 0.0, 0, "")
         self.qtgui_freq_sink_x_1.enable_autoscale(False)
         self.qtgui_freq_sink_x_1.enable_grid(True)
-        self.qtgui_freq_sink_x_1.set_fft_average(0.1)
+        self.qtgui_freq_sink_x_1.set_fft_average(0.2)
         self.qtgui_freq_sink_x_1.enable_axis_labels(True)
         self.qtgui_freq_sink_x_1.enable_control_panel(True)
         self.qtgui_freq_sink_x_1.set_fft_window_normalized(True)
 
+        self.qtgui_freq_sink_x_1.disable_legend()
 
 
         labels = ['', '', '', '', '',
@@ -185,7 +189,11 @@ class qo100(gr.top_block, Qt.QWidget):
             self.qtgui_freq_sink_x_1.set_line_alpha(i, alphas[i])
 
         self._qtgui_freq_sink_x_1_win = sip.wrapinstance(self.qtgui_freq_sink_x_1.qwidget(), Qt.QWidget)
-        self.top_layout.addWidget(self._qtgui_freq_sink_x_1_win)
+        self.top_grid_layout.addWidget(self._qtgui_freq_sink_x_1_win, 15, 0, 14, 1)
+        for r in range(15, 29):
+            self.top_grid_layout.setRowStretch(r, 1)
+        for c in range(0, 1):
+            self.top_grid_layout.setColumnStretch(c, 1)
         self.limesdr_source_0 = limesdr.source('', 0, '')
         self.limesdr_source_0.set_sample_rate(samp_rate)
         self.limesdr_source_0.set_center_freq(739.75e6, 0)
@@ -205,16 +213,24 @@ class qo100(gr.top_block, Qt.QWidget):
         self.blocks_multiply_xx_0_0_0_0 = blocks.multiply_vcc(1)
         self.blocks_multiply_xx_0_0_0 = blocks.multiply_vcc(1)
         self.blocks_multiply_const_vxx_1 = blocks.multiply_const_cc(0.01)
-        self.blocks_multiply_const_vxx_0_0 = blocks.multiply_const_ff(10)
-        self.blocks_multiply_const_vxx_0 = blocks.multiply_const_ff(af_gain)
         self.blocks_float_to_complex_1_0 = blocks.float_to_complex(1)
         self.blocks_complex_to_float_0_0 = blocks.complex_to_float(1)
         self.blocks_complex_to_float_0 = blocks.complex_to_float(1)
         self.blocks_add_xx_0 = blocks.add_vcc(1)
-        self.band_pass_filter_0_0_0 = filter.fir_filter_ccc(
-            1,
+        self.band_pass_filter_0_0_1 = filter.fir_filter_ccc(
+            2*decim,
             firdes.complex_band_pass(
                 1,
+                samp_rate,
+                -12e3,
+                12e3,
+                1000,
+                window.WIN_HAMMING,
+                6.76))
+        self.band_pass_filter_0_0_0 = filter.fir_filter_ccc(
+            decim,
+            firdes.complex_band_pass(
+                10,
                 samp_rate,
                 0,
                 3000,
@@ -222,16 +238,16 @@ class qo100(gr.top_block, Qt.QWidget):
                 window.WIN_HAMMING,
                 6.76))
         self.band_pass_filter_0_0 = filter.fir_filter_ccc(
-            1,
+            decim,
             firdes.complex_band_pass(
-                1,
+                af_gain,
                 samp_rate,
                 0,
                 3000,
                 1000,
                 window.WIN_HAMMING,
                 6.76))
-        self.band_pass_filter_0 = filter.fir_filter_ccc(
+        self.band_pass_filter_0 = filter.interp_fir_filter_ccc(
             1,
             firdes.complex_band_pass(
                 1,
@@ -259,27 +275,24 @@ class qo100(gr.top_block, Qt.QWidget):
         self.connect((self.analog_sig_source_x_0_0_0, 0), (self.blocks_multiply_xx_0_0_0_0, 1))
         self.connect((self.analog_sig_source_x_0_0_0_0, 0), (self.blocks_multiply_xx_0_0_0_0_0, 1))
         self.connect((self.audio_source_0, 0), (self.blocks_float_to_complex_1_0, 0))
-        self.connect((self.band_pass_filter_0, 0), (self.rational_resampler_xxx_0_0, 0))
-        self.connect((self.band_pass_filter_0_0, 0), (self.rational_resampler_xxx_0, 0))
-        self.connect((self.band_pass_filter_0_0_0, 0), (self.rational_resampler_xxx_0_1, 0))
+        self.connect((self.band_pass_filter_0, 0), (self.rational_resampler_xxx_0, 0))
+        self.connect((self.band_pass_filter_0_0, 0), (self.blocks_complex_to_float_0, 0))
+        self.connect((self.band_pass_filter_0_0_0, 0), (self.blocks_complex_to_float_0_0, 0))
+        self.connect((self.band_pass_filter_0_0_1, 0), (self.qtgui_freq_sink_x_1, 0))
         self.connect((self.blocks_add_xx_0, 0), (self.qtgui_waterfall_sink_x_0, 0))
-        self.connect((self.blocks_complex_to_float_0, 0), (self.blocks_multiply_const_vxx_0, 0))
-        self.connect((self.blocks_complex_to_float_0_0, 0), (self.blocks_multiply_const_vxx_0_0, 0))
+        self.connect((self.blocks_complex_to_float_0, 0), (self.audio_sink_0, 0))
+        self.connect((self.blocks_complex_to_float_0_0, 0), (self.audio_sink_0_0, 0))
         self.connect((self.blocks_float_to_complex_1_0, 0), (self.band_pass_filter_0, 0))
-        self.connect((self.blocks_multiply_const_vxx_0, 0), (self.audio_sink_0, 0))
-        self.connect((self.blocks_multiply_const_vxx_0_0, 0), (self.audio_sink_0_0, 0))
         self.connect((self.blocks_multiply_const_vxx_1, 0), (self.blocks_swapiq_0, 0))
         self.connect((self.blocks_multiply_xx_0_0_0, 0), (self.limesdr_sink_0, 0))
         self.connect((self.blocks_multiply_xx_0_0_0_0, 0), (self.band_pass_filter_0_0, 0))
+        self.connect((self.blocks_multiply_xx_0_0_0_0, 0), (self.band_pass_filter_0_0_1, 0))
         self.connect((self.blocks_multiply_xx_0_0_0_0_0, 0), (self.band_pass_filter_0_0_0, 0))
         self.connect((self.blocks_swapiq_0, 0), (self.blocks_add_xx_0, 1))
         self.connect((self.limesdr_source_0, 0), (self.blocks_add_xx_0, 0))
         self.connect((self.limesdr_source_0, 0), (self.blocks_multiply_xx_0_0_0_0, 0))
         self.connect((self.limesdr_source_0, 0), (self.blocks_multiply_xx_0_0_0_0_0, 0))
-        self.connect((self.rational_resampler_xxx_0, 0), (self.blocks_complex_to_float_0, 0))
-        self.connect((self.rational_resampler_xxx_0, 0), (self.qtgui_freq_sink_x_1, 0))
-        self.connect((self.rational_resampler_xxx_0_0, 0), (self.blocks_multiply_xx_0_0_0, 0))
-        self.connect((self.rational_resampler_xxx_0_1, 0), (self.blocks_complex_to_float_0_0, 0))
+        self.connect((self.rational_resampler_xxx_0, 0), (self.blocks_multiply_xx_0_0_0, 0))
 
 
     def closeEvent(self, event):
@@ -289,6 +302,13 @@ class qo100(gr.top_block, Qt.QWidget):
         self.wait()
 
         event.accept()
+
+    def get_decim(self):
+        return self.decim
+
+    def set_decim(self, decim):
+        self.decim = decim
+        self.set_samp_rate(self.decim*48e3)
 
     def get_vfo(self):
         return self.vfo
@@ -306,8 +326,9 @@ class qo100(gr.top_block, Qt.QWidget):
         self.analog_sig_source_x_0_0.set_sampling_freq(self.samp_rate)
         self.analog_sig_source_x_0_0_0.set_sampling_freq(self.samp_rate)
         self.analog_sig_source_x_0_0_0_0.set_sampling_freq(self.samp_rate)
-        self.band_pass_filter_0_0.set_taps(firdes.complex_band_pass(1, self.samp_rate, 0, 3000, 1000, window.WIN_HAMMING, 6.76))
-        self.band_pass_filter_0_0_0.set_taps(firdes.complex_band_pass(1, self.samp_rate, 0, 3000, 1000, window.WIN_HAMMING, 6.76))
+        self.band_pass_filter_0_0.set_taps(firdes.complex_band_pass(self.af_gain, self.samp_rate, 0, 3000, 1000, window.WIN_HAMMING, 6.76))
+        self.band_pass_filter_0_0_0.set_taps(firdes.complex_band_pass(10, self.samp_rate, 0, 3000, 1000, window.WIN_HAMMING, 6.76))
+        self.band_pass_filter_0_0_1.set_taps(firdes.complex_band_pass(1, self.samp_rate, -12e3, 12e3, 1000, window.WIN_HAMMING, 6.76))
         self.qtgui_waterfall_sink_x_0.set_frequency_range(250e3, self.samp_rate)
 
     def get_mag(self):
@@ -324,7 +345,7 @@ class qo100(gr.top_block, Qt.QWidget):
 
     def set_af_gain(self, af_gain):
         self.af_gain = af_gain
-        self.blocks_multiply_const_vxx_0.set_k(self.af_gain)
+        self.band_pass_filter_0_0.set_taps(firdes.complex_band_pass(self.af_gain, self.samp_rate, 0, 3000, 1000, window.WIN_HAMMING, 6.76))
 
 
 
