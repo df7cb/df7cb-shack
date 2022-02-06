b747b6962811355841c6396fc2a926111c953429
Christoph Berg <myon@debian.org>
2022-02-06 23:08:56 +0100

    cw: Ramp up CW signal instead of using a bandpass

--- gnuradio/cw_cw_modulator.py
+++ gnuradio/cw_cw_modulator.py
@@ -1,9 +1,5 @@
 """
-Embedded Python Blocks:
-
-Each time this file is saved, GRC will instantiate the first class it finds
-to get ports and parameters of your block. The arguments to __init__  will
-be the parameters. All of them are required to have default values!
+CW Modulator Block
 """
 
 import numpy as np
@@ -11,24 +7,34 @@ from gnuradio import gr
 import pmt
 
 class blk(gr.sync_block):  # other base classes are basic_block, decim_block, interp_block
-    """Embedded Python Block example - a simple multiply const"""
+    """CW Modulator block"""
 
-    def __init__(self):
+    def __init__(self, transition=1000):
         """arguments to this function show up as parameters in GRC"""
         gr.sync_block.__init__(
             self,
-            name='Embedded Python Block',   # will show up in GRC
+            name='CW Modulator',   # will show up in GRC
             in_sig=[np.float32],
             out_sig=[np.float32]
         )
-        self.message_port_register_in(pmt.intern("cw_in"))
-        self.set_msg_handler(pmt.intern('cw_in'), self.key)
-        self.keyed = 0.0
+        # ramp up signal over this many ticks
+        self.transition = transition
+        self.level = 0
+        self.keyed = False
+
+        self.message_port_register_in(pmt.intern("key"))
+        self.set_msg_handler(pmt.intern('key'), self.key)
 
     def key(self, msg):
-        if pmt.car(msg) == pmt.intern('cw'):
-            self.keyed = pmt.to_float(pmt.cdr(msg))
+        if pmt.is_pair(msg) and pmt.car(msg) == pmt.intern('key'):
+            self.keyed = pmt.to_bool(pmt.cdr(msg))
 
     def work(self, input_items, output_items):
-        output_items[0][:] = input_items[0] * self.keyed
+        for i in range(len(output_items[0])):
+            if self.keyed and self.level < self.transition:
+                self.level += 1
+            if not self.keyed and self.level > 0:
+                self.level -= 1
+            factor = 0.5 * (1 - np.cos(np.pi * self.level / self.transition))
+            output_items[0][i] = input_items[0][i] * factor
         return len(output_items[0])
--- gnuradio/cw_cw_source.py
+++ gnuradio/cw_cw_source.py
@@ -1,25 +1,17 @@
-"""
-Embedded Python Blocks:
-
-Each time this file is saved, GRC will instantiate the first class it finds
-to get ports and parameters of your block. The arguments to __init__  will
-be the parameters. All of them are required to have default values!
-"""
-
 import numpy as np
 from gnuradio import gr
 import pmt
 import threading
 import serial
 
-class blk(gr.sync_block):  # other base classes are basic_block, decim_block, interp_block
-    """Embedded Python Block example - a simple multiply const"""
+class blk(gr.sync_block):
+    """Read CW input from modified K3NG keyer"""
 
     def __init__(self, port='/dev/ttyUSBK3NG'):  # only default arguments here
         """arguments to this function show up as parameters in GRC"""
         gr.sync_block.__init__(
             self,
-            name='CW Source',   # will show up in GRC
+            name='CW Source',
             in_sig=None,
             out_sig=None,
         )
@@ -27,10 +19,7 @@ class blk(gr.sync_block):  # other base classes are basic_block, decim_block, in
         # a callback is registered (properties work, too).
         self.port = port
 
-        #self.message_port_register_in(pmt.intern("freq_in"))
-        #self.message_port_register_out(pmt.intern("freq_out"))
-        self.message_port_register_out(pmt.intern("cw_out"))
-        #self.set_msg_handler(pmt.intern('freq_in'), self.freq_in)
+        self.message_port_register_out(pmt.intern("key"))
 
     def start(self):
         self.serial = serial.Serial(self.port, 1200)
@@ -38,16 +27,16 @@ class blk(gr.sync_block):  # other base classes are basic_block, decim_block, in
         self.cwthread = threading.Thread(target=self.cw, daemon=True)
         self.cwthread.start()
 
+    def set_key(self, key):
+        self.message_port_pub(pmt.intern("key"),
+                pmt.cons(pmt.intern("key"), pmt.from_bool(key)))
+
     def cw(self):
         while True:
             try:
                 x = self.serial.read()
+                if x in (b'^', b'_'):
+                    self.set_key(x == b'^')
             except Exception as e:
-                self.message_port_pub(pmt.intern("cw_out"),
-                        pmt.cons(pmt.intern('cw'), pmt.from_float(0.0)))
-                self.message_port_pub(pmt.intern("cw_out"),
-                        pmt.cons(pmt.intern('error'), pmt.string_to_symbol(str(e))))
-            if x in (b'^', b'_'):
-                ampl = 0.5 if x == b'^' else 0.0
-                self.message_port_pub(pmt.intern("cw_out"),
-                        pmt.cons(pmt.intern('cw'), pmt.from_float(ampl)))
+                self.set_key(False) # turn signal off on error
+                print(e)
